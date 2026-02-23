f6e94c5a1326be6800c246ff71be03c178f51b82
Pavel Mareš <mares.pa@protonmail.com>
2026-02-23 19:37:43 +0100

    Funkce: Napojení k SerpApi, načtení surových dat Google

--- scraper.py
+++ scraper.py
@@ -1,11 +1,20 @@
 import os # Načtení základní knihovny pro komunikaci s operačním systémem
+import requests # Načtení knihovny pro HTTP požadavky
 from dotenv import load_dotenv # Načtení funkce z knihovny - prostředí z .env souboru
 
 load_dotenv() # Načtení proměnných prostředí z .env souboru do systému
 
 api_key = os.getenv("API_KEY") # Načtení hodnoty API_KEY z prostředí, které bylo načteno z .env souboru
 
-if api_key: # Kontrola úspěšnosti použití API klíče
-    print("Super! Klíč byl úspěšně načten a je připraven k použití.")
-else:
-    print("Chyba: Klíč nebyl nalezen. Zkontroluj soubor .env!")
+url = "https://serpapi.com/search" # URL pro API SerpApi
+
+params = {
+    "engine": "google",
+    "q": "Pavel Mareš Inizio",
+    "api_key": api_key
+} # Parametry pro API požadavek, včetně typu vyhledávače (Google), hledaného dotazu ("Pavel Mareš Inizio") a API klíče pro autentizaci
+
+response = requests.get(url, params=params) # Odeslání GET požadavku na API SerpApi s danými parametry
+results = response.json() # Převod odpovědi z JSON formátu do Python slovníku
+
+print(results) # Výpis získaných výsledků na konzoli
